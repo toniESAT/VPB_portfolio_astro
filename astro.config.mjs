8ff21174183ddebac27491c0c9912e213d48c843
toniESAT <martinezbro@esat-alumni.com>
2024-04-12 17:24:23 +0200

    Config domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://toniesat.github.io',
+  base: 'VPB_portfolio_astro',
+})
