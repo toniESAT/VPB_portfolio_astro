09055e98b201622caf514578491d0e9e853d9a10
toniESAT <martinezbro@esat-alumni.com>
2024-05-04 15:18:12 +0200

    Add privacy, improve footer, test root as base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://toniesat.github.io',
-  base: 'VPB_portfolio_astro',
+  base: '/',
 })
