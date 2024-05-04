b691d767ed91a4a552db785459ed949993c7aa2f
toniESAT <martinezbro@esat-alumni.com>
2024-05-04 17:11:13 +0200

    reduce image size, deploy to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://toniesat.github.io',
-  base: '/',
+  site: "https://victorperezbrotons.com",
+  output: "static",
+  compressHTML: false // Temporary, remove
+  // build: { format: "file"}
 })
